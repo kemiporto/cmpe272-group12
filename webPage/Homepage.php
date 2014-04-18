b83d73af7d9858539b0b0e6420ab70b761049bab
Van Liao <VanLiao@VanLiao.local>
2014-04-18 01:50:12 -0700

    add new page

---
+++ webPage/Homepage.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Homepage</title>
+        <script type="text/javascript">
+            function navigateTo()
+            {
+                window.location="SelectTablePage.php";
+            }
+        </script>
+    </head>
+
+    <body>
+        <div>
+            <div style="font-weight:bold;text-align:center">Menu</div>
+            <!--<div><hr/></div>-->
+            <!--<div><a href="http://www.google.com">goto google</a></div>-->
+            <div style="text-align:center"><input type="button" value="Add Data" onclick="navigateTo()"/>
+               
+            </div>
+
+            <div style="text-align:center"><input type="button" value="View Crowd Source Data" onclick="navigateTo()"  />
+                
+            </div>
+
+        </div>
+    </body>
+</html>
