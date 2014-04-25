36a6074050ccd6cc6890a76497f476c48c218e6b
Darrel Pham <drlqpham@gmail.com>
2014-04-24 21:44:48 -0700

    Making Select Table Page pretty

--- webPage/Homepage.php
+++ webPage/Homepage.php
@@ -5,7 +5,8 @@
 <html>
 	
     <head>
-    	<script src="js/jquery.js"></script> 
+    	<script src="js/jquery.js"></script>
+    	<script> $(function(){ $("#titleBar").load("TitleBar.html"); } ); </script>  
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <script type="text/javascript">
             function navigateTo()
@@ -19,7 +20,6 @@
                 window.location="Homepage.php";
             }
         </script>      	
-    	<script> $(function(){ $("#titleBar").load("TitleBar.html"); } ); </script> 
     </head>   
 
     <body> 
@@ -27,15 +27,16 @@
     </body>
 
     <body>
-        <div>
+        <div>            
+			<div class="container" style="max-width:300px">
 			<h1> <div style="font-weight:bold; text-align:center">Menu</div> </h1>
-			<hr attribute1=".." attribute2="..">
-            
-			<div class="col-md-2" >
-				<!--the category section-->
-				<div class="list-group center-block" style="text-align:center">
-					<a href="SelectTablePage.php" data-toggle="modal" class="list-group-item">Add Data</a>
-					<a href="SelectTablePage.php" class="list-group-item">View Crowd Source Data</a>					
+			     
+	    		<div class="span12" >    
+					<!--the category section-->
+					<div class="list-group center-block" style="text-align:center">
+						<a href="SelectTablePage.php" data-toggle="modal" class="list-group-item">Add Data</a>
+						<a href="SelectTablePage.php" class="list-group-item">View Crowd Source Data</a>					
+					</div>
 				</div>
 			</div>
 
--- webPage/SelectTablePage.php
+++ webPage/SelectTablePage.php
@@ -5,74 +5,75 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
+	
+	<!-- Includes -->
     <head>
-        <style>
-            .head{
-                text-align: center;
+    	<script src="js/jquery.js"></script>
+    	<script> $(function(){ $("#titleBar").load("TitleBar.html"); } ); </script>  
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <script type="text/javascript">
+            function navigateBack()
+            {
+                window.location="Homepage.php";
             }
+        </script>      	
+    </head>
 
-            #firstTable{
-                column-width: 500px;
-                color: red;
-                text-align: center;
-
-            } 
-            .select{
-                width: 150px;
-                height: 300px;
-            }
-            td{
-                padding: 15px;
-                height:15px;
-                text-align: center;
-            }
-            .div{
-                width: 500px;
-                margin-right: auto;
-                margin-left: auto;
-                
-            }
-            .TableAlignCenter{
-                 margin-right: auto;
-                margin-left: auto;
-            }
-
-        </style>
-        <meta charset="UTF-8">
-        <title>Head</title>
-    <div class="head">    
-        <h1 >HEAD </h1></div>
-</head> 
-<body>
-    <div class="div">
-        <table class="TableAlignCenter">
-        <tr>
-            <td>Source1</td>
-            <td>Source2</td>
-        </tr>
-        <tr>
-            <td>
-                <select class="select"  id="firstTable" name="listname" multiple="multiple">
-                    <option>Table1</option>
-                    <option>Table2</option>
-                    <option>Table3</option>
-                    <option>Table4</option>
-                    <option>Table5</option>
-                </select>
-            </td><td>
-                <select class="select" id="firstTable" name="listname" multiple="multiple">
-                    <option>Table1</option>
-                    <option>Table2</option>
-                    <option>Table3</option>
-                    <option>Table4</option>
-                    <option>Table5</option>
-                </select>
-            </td>
-        </tr>
-
-    </table>
-        <div style="text-align: center;margin-right: auto;
-                margin-left: auto;"> <input  type ="button" onclick="" value="View" /></div>
-    </div>
+    <body> 
+     <div id="titleBar"></div>
+    </body>
+    
+    
+    <body>
+        <div>
+			<h1> <div style="font-weight:bold; text-align:center">Select Table</div> </h1>
+        </div>
+    </body>
+    
+    <div class="container" style="max-width:600px">     
+	    <div class="span12">      
+			<div>
+				<div class="panel panel-default">
+  					<!-- Default panel contents -->
+  					<div class="panel-heading" style="text-align: center">Choose Sources</div>
+						<!-- Table -->		
+  						<table class="table table-striped table-bordered table-condensed" style="float: center; ">
+					        <tr>
+					            <td>
+					                <select class="select"  id="firstTable" name="listname" multiple="multiple" style="float:left; margin-left: 10%; min-width: 220px; min-height: 300px;">
+					                    <option>Table1</option>
+					                    <option>Table2</option>
+					                    <option>Table3</option>
+					                    <option>Table4</option>
+					                    <option>Table5</option>
+					                </select>
+					            </td>
+					            <td>
+					                <select class="select" id="secondTable" name="listname" multiple="multiple" style="float:right;margin-right: 10%; min-width: 220px; min-height: 300px;">
+					                    <option>Table1</option>
+					                    <option>Table2</option>
+					                    <option>Table3</option>
+					                    <option>Table4</option>
+					                    <option>Table5</option>
+					                </select>
+					            </td>
+					        </tr>    
+  						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+            
+    <div class="container" style="max-width:300px">     
+	    <div class="span12">      
+			<div>
+				<!--the category section-->
+				<div class="list-group center-block" style="text-align:center">
+					<a href="ViewChat.php" data-toggle="modal" class="list-group-item">View</a>			
+				</div>
+			</div>
+	    </div> 
+	</div>
 </body>
 </html>
