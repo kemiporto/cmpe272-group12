2cf84b255fcfe58264088136204905a257334750
Darrel Pham <drlqpham@gmail.com>
2014-04-24 20:35:16 -0700

    Make homepage pretty

--- webPage/Homepage.php
+++ webPage/Homepage.php
@@ -1,27 +1,48 @@
+ 
+    
+
+	
 <html>
+	
     <head>
-        <title>Homepage</title>
+    	<script src="js/jquery.js"></script> 
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <script type="text/javascript">
             function navigateTo()
             {
                 window.location="SelectTablePage.php";
             }
         </script>
-    </head>
+        <script type="text/javascript">
+            function navigateBack()
+            {
+                window.location="Homepage.php";
+            }
+        </script>      	
+    	<script> $(function(){ $("#titleBar").load("TitleBar.html"); } ); </script> 
+    </head>   
+
+    <body> 
+     <div id="titleBar"></div>
+    </body>
 
     <body>
         <div>
-            <div style="font-weight:bold;text-align:center">Menu</div>
-            <!--<div><hr/></div>-->
-            <!--<div><a href="http://www.google.com">goto google</a></div>-->
-            <div style="text-align:center"><input type="button" value="Add Data" onclick="navigateTo()"/>
-               
-            </div>
-
-            <div style="text-align:center"><input type="button" value="View Crowd Source Data" onclick="navigateTo()"  />
-                
-            </div>
+			<h1> <div style="font-weight:bold; text-align:center">Menu</div> </h1>
+			<hr attribute1=".." attribute2="..">
+            
+			<div class="col-md-2" >
+				<!--the category section-->
+				<div class="list-group center-block" style="text-align:center">
+					<a href="SelectTablePage.php" data-toggle="modal" class="list-group-item">Add Data</a>
+					<a href="SelectTablePage.php" class="list-group-item">View Crowd Source Data</a>					
+				</div>
+			</div>
 
         </div>
+    </body>    
+
+    <body>
     </body>
+		
 </html>
